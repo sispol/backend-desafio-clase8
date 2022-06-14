b818f42f3feaf8664680b9c5bcb557a901ae1133
Oscar Pereyra <oscarlper@gmail.com>
2022-06-14 19:50:56 -0300

    Desafio clase 8

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = Router()
 
 router.get('/api/productos',(req,res) => {
     if (db.allItems() >0) {
-        res.status(200).json(db.getAll())
+        res.status(201).json(db.getAll())
     } else {
         return res.status(404).json({error: "no se encontraron productos"});
     }
@@ -14,30 +14,55 @@ router.get('/api/productos/:id',(req,res) => {
     if (db.getById(id) === undefined) {
         res.status(404).json({error: "producto no encontrado"})
     } else {
-        res.status(200).json(db.getById(id))
+        res.status(201).json(db.getById(id))
     }
 })
 
 router.post('/api/productos',(req,res) => {
-    id = db.lastId()+1
-    const { title, price, thumbnail } = req.body
-    db.producto.push({id,title,price,thumbnail})
-    res.status(200).json(db.getById(id))
-    res.sendStatus(201)
+    console.log('lastid:'+  db.lastId.id)
+    if (db.lastId.id === undefined) {
+        id=0
+    } else {
+        id = db.lastId()+1
+    }
+        const { title, price, thumbnail } = req.body
+        db.producto.push({id,title,price,thumbnail})
+        res.status(201).json(db.getById(id))
 })
 
-// PENSAR ESTO !!!
-router.put('/api/productos',(req,res) => {
-    const { id } = req.body
-    allItemValue = db.allItems()-1
-    db.producto.splice(1, allItemValue, {title: 1020});
-    res.sendStatus(201)
+// Envio el id por metodo put y le cambio el price a sin stock.
+router.put('/api/productos/:id',(req,res) => {
+    id = req.params.id
+    let resultado = "";
+    let i;
+
+    for (i = 0; i < db.allItems() ; i++) {
+        if (Number(id) === db.producto[i].id) {
+            console.log(`Producto: ${db.producto[i].id} encontrado`);
+            resultado = 'encontrado';
+            db.producto.splice(i, 1, {
+                                        id: db.producto[i].id,
+                                        title: db.producto[i].title,
+                                        price: 'SIN STOCK',
+                                        thumnail: db.producto[i].title
+                                    });
+            res.status(201).json(db.getById(id))
+        } 
+    }
+
+    if (resultado !== 'encontrado') {
+        res.status(404).json({ error : 'producto no encontrado' })
+    }
 })
 
 router.delete('/api/productos/:id',(req,res) => {
-    const { nombre, apellido, edad} = req.body
-    mascotas.push({nombre,apellido,edad})
-    res.sendStatus(201)
+    id = req.params.id
+    if (db.getById(id) !== undefined) {
+        db.producto = (db.producto.filter(buscaId => buscaId.id !== Number(id)))
+        res.status(201).json(db.producto)
+    } else {
+        res.status(404).json({ error : 'producto no encontrado' })
+    }
 })
 
 class Contenedor {
@@ -57,7 +82,6 @@ class Contenedor {
     deleteAll() {
             productos = []
             return this.producto
-//            return res.status(404).json({message: "Hubo un error de escritura: ",e});
     }
 
     allItems() {
@@ -74,6 +98,6 @@ let id;
 const producto = []
 
 const db = new Contenedor();
-db.producto = [{"id":1,"title":"prodNumber387","price":38700,"thumbnail":"http://thumbnail_387"},{"id":2,"title":"prodNumber444","price":44400,"thumbnail":"http://thumbnail_444"},{"id":3,"title":"prodNumber641","price":64100,"thumbnail":"http://thumbnail_641"}]
-
+//db.producto = [{"id":1,"title":"prodNumber387","price":38700,"thumbnail":"http://thumbnail_387"},{"id":2,"title":"prodNumber444","price":44400,"thumbnail":"http://thumbnail_444"},{"id":3,"title":"prodNumber641","price":64100,"thumbnail":"http://thumbnail_641"}]
+db.producto = []
 module.exports = router
